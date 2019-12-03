1dfb90e9ab3f370d31540b4eac3839b66e449720
1612719 <danghuyentram@gmail.com>
2019-12-03 22:17:49 +0700

    add_cost

---
+++ app/src/main/java/com/hoaithi/tripme/MyPlan.java
@@ -0,0 +1,14 @@
+package com.hoaithi.tripme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_plan);
+    }
+}
