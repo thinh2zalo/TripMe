8d4c20473366d5a935ff42fc1a4f20c3af38e071
htn274 <thiennust274@gmail.com>
2019-12-09 03:09:56 +0700

    search place

--- app/src/main/java/com/hoaithi/tripme/PlaceApi.java
+++ app/src/main/java/com/hoaithi/tripme/PlaceApi.java
@@ -1,6 +1,10 @@
 package com.hoaithi.tripme;
 
+import android.os.AsyncTask;
 import android.util.Log;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.hoaithi.tripme.model.Attraction;
 
@@ -10,13 +14,14 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class PlaceApi {
-    final String API_KEY = "AIzaSyA3gCqHV2M456HOm_35h9MiGSP_7NgPrnE";
+    static final String API_KEY = "AIzaSyA3gCqHV2M456HOm_35h9MiGSP_7NgPrnE";
     public ArrayList<String> autoComplete(String input){
         ArrayList<String> arrayList=new ArrayList();
         HttpURLConnection connection=null;
@@ -67,62 +72,4 @@ public class PlaceApi {
 
         return arrayList;
     }
-
-    public ArrayList<Attraction> getAttractionInCity(String city)
-    {
-        ArrayList<Attraction> arrayList=new ArrayList();
-        HttpURLConnection connection=null;
-        StringBuilder jsonResult=new StringBuilder();
-        try {
-            StringBuilder sb=new StringBuilder("https://maps.googleapis.com/maps/api/place/textsearch/json?");
-            sb.append("query="+city.replaceAll("\\s+","")+"+city+point+of+interest&language=vi&key="+API_KEY);
-            Log.d("Nunu", sb.toString());
-            URL url=new URL(sb.toString());
-            connection=(HttpURLConnection)url.openConnection();
-            InputStreamReader inputStreamReader=new InputStreamReader(connection.getInputStream());
-            int read;
-
-            char[] buff=new char[1024];
-            while ((read=inputStreamReader.read(buff))!=-1){
-                jsonResult.append(buff,0,read);
-            }
-        }
-        catch (MalformedURLException e){
-            e.printStackTrace();
-            Log.d("Nunu", e.getMessage());
-        }
-        catch (IOException e){
-            e.printStackTrace();
-            Log.d("Nunu", e.getLocalizedMessage());
-        }
-        finally {
-            if(connection!=null){
-                connection.disconnect();
-            }
-        }
-
-        try {
-            JSONObject jsonObject=new JSONObject(jsonResult.toString());
-            JSONArray results=jsonObject.getJSONArray("results");
-            Log.d("Nunu", Integer.toString(results.length()));
-            for(int i=0;i< results.length();i++){
-                JSONObject objecti = results.getJSONObject(i);
-                String name = objecti.getString("name");
-                String address = objecti.getString("formatted_address");
-                JSONObject location = objecti.getJSONObject("geometry").getJSONObject("location");
-                String photo = objecti.getJSONArray("photos").getJSONObject(0).getString("photo_reference");
-                String rating = objecti.getString("rating");
-                arrayList.add(new Attraction(name, photo, address, location.getString("lat"), location.getString("lng"),
-                        rating));
-            }
-        }
-        catch (JSONException e){
-            e.printStackTrace();
-        }
-
-
-        return arrayList;
-    }
-
-    
 }
--- app/src/main/java/com/hoaithi/tripme/SearchPlaceActivity.java
+++ app/src/main/java/com/hoaithi/tripme/SearchPlaceActivity.java
@@ -11,10 +11,21 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.hoaithi.tripme.model.Attraction;
+import com.hoaithi.tripme.model.Place;
 import com.hoaithi.tripme.util.Util;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -23,18 +34,17 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class SearchPlaceActivity extends AppCompatActivity {
+public class SearchPlaceActivity extends AppCompatActivity  {
 
     @BindView(R.id.status_bar)
     View mStatusBar;
-
+    @BindView(R.id.city_text_view)
+    TextView cityTextView;
 
     @BindView(R.id.attraction_recycler_view)
     RecyclerView attractionRecyclerView;
-
     AttractionItemAdapter attractionItemAdapter;
     List<Attraction> attractionList = new ArrayList<>();
-    PlaceApi placeApi;
 
     @OnClick(R.id.back_button)
     void back() {
@@ -53,15 +63,7 @@ public class SearchPlaceActivity extends AppCompatActivity {
         // add status bar space
         mStatusBar.getLayoutParams().height = Util.getStatusHeight(getResources());
         mStatusBar.requestLayout();
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        getData();
         initView();
-    }
-
-    private void getData() {
-        attractionList = placeApi.getAttractionInCity("Đà Lạt");
     }
 
     private void initView() {
@@ -72,6 +74,75 @@ public class SearchPlaceActivity extends AppCompatActivity {
         attractionRecyclerView.setLayoutManager(layoutManager);
         attractionRecyclerView.setItemAnimator(new DefaultItemAnimator());
         attractionRecyclerView.setAdapter(attractionItemAdapter);
+
+        new getAttractionInCity(cityTextView.getText().toString());
+    }
+
+    private class  getAttractionInCity  extends AsyncTask<String, Integer, String>
+    {
+        StringBuilder sb;
+
+        public getAttractionInCity(String city)
+        {
+            sb=new StringBuilder("https://maps.googleapis.com/maps/api/place/textsearch/json?");
+            sb.append("query="+city.replaceAll("\\s+","")+"+city+point+of+interest&language=vi&key="+ PlaceApi.API_KEY);
+            this.execute(sb.toString());
+        }
+
+        @Override
+        protected String doInBackground(String... strurl) {
+            HttpURLConnection connection=null;
+
+            StringBuilder jsonResult=new StringBuilder();
+            try{
+                URL url=new URL(strurl[0]);
+                connection=(HttpURLConnection)url.openConnection();
+                InputStreamReader inputStreamReader=new InputStreamReader(connection.getInputStream());
+                int read;
+
+                char[] buff=new char[1024];
+                while ((read=inputStreamReader.read(buff))!=-1){
+                    jsonResult.append(buff,0,read);
+                }
+            }
+            catch (MalformedURLException e){
+                e.printStackTrace();
+                Log.d("Nunu", e.getMessage());
+            }
+            catch (IOException e){
+                e.printStackTrace();
+                Log.d("Nunu", e.getLocalizedMessage());
+            }
+            finally {
+                if(connection!=null){
+                    connection.disconnect();
+                }
+            }
+            return jsonResult.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject=new JSONObject(result);
+                JSONArray results=jsonObject.getJSONArray("results");
+                Log.d("Nunu", Integer.toString(results.length()));
+                for(int i=0;i< results.length();i++){
+                    JSONObject objecti = results.getJSONObject(i);
+                    String name = objecti.getString("name");
+                    String address = objecti.getString("formatted_address");
+                    JSONObject location = objecti.getJSONObject("geometry").getJSONObject("location");
+                    String photo = objecti.getJSONArray("photos").getJSONObject(0).getString("photo_reference");
+                    String rating = objecti.getString("rating");
+                    attractionList.add(new Attraction(name, photo, address, location.getString("lat"), location.getString("lng"),
+                            rating));
+                    attractionItemAdapter.notifyDataSetChanged();
+                }
+            }
+            catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
     }
 
 }
